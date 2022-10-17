924faeb2b09adb3bbec04d66b6276d5cfae92f97
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-17 16:52:54 +0000

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Tarek Chaaban';
+var isCool = true;
+var totalPets = 0;
+var unknown = null;
+var never;
+
+console.log('Full name: ', fullName);
+console.log('Is he cool?: ', isCool);
+console.log('Total pets: ', totalPets);
+
+console.log('Type of variable: ', typeof fullName);
+console.log('Type of variable: ', typeof isCool);
+console.log('Type of variable: ', typeof totalPets);
+
+console.log(unknown);
+console.log('Type of variable: ', typeof unknown);
+
+console.log(never);
+console.log('Type of variable: ', typeof never);
