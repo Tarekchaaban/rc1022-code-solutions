fdf4731a0eff771f72c5405747d82a17e58dad06
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-17 18:46:27 +0000

    Completed javascript-operators-and-expressions excercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,51 @@
+var width = 10;
+var height = 20;
+
+var area = width * height;
+
+console.log('Value of area:', area);
+console.log('Typeof area:', typeof area);
+
+var bill = 95;
+var payment = 100;
+
+var change = payment - bill;
+
+console.log('Value of change:', change);
+console.log('Typeof change:', typeof change);
+
+var quizzes = 90;
+var midterm = 100;
+var final = 95;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('Value of grade:', grade);
+console.log('Typeof grade:', typeof grade);
+
+var firstName = 'Tarek ';
+var lastName = 'Chaaban';
+
+var fullName = firstName + lastName;
+
+console.log('Value of fullName:', fullName);
+console.log('Typeof fullName', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+
+console.log('Value of isAcidic:', isAcidic);
+console.log('Typeof isAcidic:', typeof isAcidic);
+
+var headCount = 14;
+var isSparta = headCount === 300;
+
+console.log('Value of isSparta:', isSparta);
+console.log('Typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+
+motto += ' is the GOAT';
+
+console.log('Value of motto is:', motto);
+console.log('Typeof motto:', typeof motto);
