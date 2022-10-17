1c4eca866d2e624b9cacc98e35a8ecd0d1a17dc0
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-17 22:30:25 +0000

    Completed javascript-arrays exercise.

---
+++ javascript-arrays/main.js
@@ -0,0 +1,23 @@
+var colors = ['red', 'white', 'blue'];
+console.log('value of colors[0]:', colors[0]);
+console.log('value of colors[1]:', colors[1]);
+console.log('value of colors[2]:', colors[2]);
+
+console.log('America is ' + colors[0] + ', ' + colors[1] + ', ' + 'and ' + colors[2] + '.');
+
+colors[2] = 'green';
+
+console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', ' + 'and ' + colors[2] + '.');
+
+console.log('Value of colors:', colors);
+
+var students = ['Tarek', 'Amar', 'Andrew', 'Robby'];
+var numberofStudents = students.length;
+console.log('The number of students are:', numberofStudents);
+
+var lastIndex = numberofStudents - 1;
+var lastStudent = students[lastIndex];
+
+console.log('The last student in the array is:', lastStudent);
+
+console.log('The value of students:', students);
