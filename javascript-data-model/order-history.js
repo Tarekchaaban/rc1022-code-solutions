c02dc348663b0bfa93b19c8b235f1758402c35b0
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-18 19:00:31 +0000

    Javascript-data-model completed.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,3 +1,79 @@
 var orderHistory = [
-
+  {
+    order_num: 2648248,
+    date_placed: 'July 3rd 2020',
+    total: 138.93,
+    ship_to: 'JS Masher',
+    date_delivered: 'July 5th 2020',
+    items: [
+      {
+        type: 'Book',
+        name_of_product: 'The Art of Sql',
+        author: 'Stephane Faroult',
+        return_window: 'August 4th 2020',
+        price: 33.99,
+        imageUrl: null
+      },
+      {
+        type: 'Gaming Hardware',
+        name_of_product: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        return_window: 'August 4th 2020',
+        ship_to: 'JS Masher',
+        price: 94.95,
+        imageUrl: null
+      }
+    ]
+  },
+  {
+    order_num: 9059409,
+    date_placed: 'July 4th 2020',
+    total: 17.22,
+    ship_to: 'JS Masher',
+    date_delivered: 'July 7th 2020',
+    items: [
+      {
+        type: 'Gaming Hardware',
+        name_of_product: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Drive and No Lag-Gamecube Adapter',
+        author: null,
+        return_window: 'August 5th 2020',
+        price: 15.98,
+        imageUrl: null
+      }
+    ]
+  },
+  {
+    order_num: 1280257,
+    date_placed: 'July 19rd 2020',
+    total: 44.53,
+    ship_to: 'JS Masher',
+    date_delivered: 'July 20th 2020',
+    items: [
+      {
+        type: 'Book',
+        name_of_product: 'The Timeless Way of Building',
+        author: 'Christopher Alexander',
+        return_window: 'August 19th 2020',
+        price: 41.53,
+        imageUrl: null
+      }
+    ]
+  },
+  {
+    order_num: 8772232,
+    date_placed: 'August 4th 2020',
+    total: 34,
+    ship_to: 'JS Masher',
+    date_delivered: 'August 8th 2020',
+    items: [
+      {
+        type: 'Book',
+        name_of_product: 'JavaScript for impatient programmers',
+        author: 'Dr. Axel Rauschmayer',
+        return_window: 'September 7th 2020',
+        price: 31.55,
+        imageUrl: null
+      }
+    ]
+  }
 ];
