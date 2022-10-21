d51a3af0f030cc88cdc046efa55bd16c4fac981b
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-21 16:32:27 +0000

    fixed the add-suffix-to-all portion.

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -2,8 +2,8 @@
 function addSuffixToAll(words, suffix) {
   var suffixArray = [];
   for (var i = 0; i < words.length; i++) {
-    words[i] += suffix;
-    suffixArray.push(words[i]);
+    var x = words[i] + suffix;
+    suffixArray.push(x);
   }
   return suffixArray;
 }
