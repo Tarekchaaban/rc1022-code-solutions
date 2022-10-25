3f64e507e8753ccce64e282cd7d8c793d4b12277
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-25 18:55:02 +0000

    new turn in with requested adjustments made.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,13 +1,16 @@
 var $body = document.querySelector('.body-white');
 var $circle = document.querySelector('.yellow-circle');
+var isLightOn = true;
 
 function handleColorChange(event) {
-  if ($circle.className === 'yellow-circle') {
+  if (isLightOn === true) {
     $body.className = 'body-black';
     $circle.className = 'charcoal-circle';
+    isLightOn = false;
   } else {
     $body.className = 'body-white';
     $circle.className = 'yellow-circle';
+    isLightOn = true;
   }
 }
 
