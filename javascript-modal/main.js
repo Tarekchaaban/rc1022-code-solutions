e6177232d39b98ad53541e0b22869ec1514ab1a6
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-25 23:45:21 +0000

    completed with changes.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -2,23 +2,18 @@ var $modal = document.querySelector('.invisible');
 var $modalButton = document.querySelector('.open-modal');
 var $overlay = document.querySelector('.no-overlay');
 var $redButton = document.querySelector('.no-button');
-var isModalOpen = false;
 
-function modalHandler(event) {
-  if (isModalOpen === false) {
-    $modal.className = 'visible';
-    $overlay.className = 'overlay';
-    isModalOpen = true;
-  }
+function showModal(event) {
+  $modal.className = 'visible';
+  $overlay.className = 'overlay';
+
 }
 
-function modalHandlerTwo(event) {
-  if (isModalOpen === true) {
-    $modal.className = 'invisible';
-    $overlay.className = 'no-overlay';
-    isModalOpen = false;
-  }
+function hideModal(event) {
+  $modal.className = 'invisible';
+  $overlay.className = 'no-overlay';
+
 }
 
-$modalButton.addEventListener('click', modalHandler);
-$redButton.addEventListener('click', modalHandlerTwo);
+$modalButton.addEventListener('click', showModal);
+$redButton.addEventListener('click', hideModal);
