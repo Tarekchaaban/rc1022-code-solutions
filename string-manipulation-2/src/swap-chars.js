97af16e7016170054a3426f174b881109d379b30
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-27 02:29:34 +0000

    work in progress 10/26 7:29 pm

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,7 +1,14 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
-  var temp = string[firstIndex];
-  string[firstIndex] = string[secondIndex];
-  string[secondIndex] = temp;
-  return string;
+  var swappedString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      swappedString += string[secondIndex];
+    } else if (i === secondIndex) {
+      swappedString += string[firstIndex];
+    } else {
+      swappedString += string[i];
+    }
+  }
+  return swappedString;
 }
