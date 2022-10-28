bd60d7a87944254ac77d8bf1f30379b2e46d1d61
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-28 20:48:38 +0000

    array-manipulation-2 work in progress. two more left.

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,4 +1,8 @@
 /* exported includes */
+/* first do the conditional to check if the array is empty, if so return false.then use the indexOf method
+on the area to check if the index is present. if the index is present the return will not equal zero - therefore return true.
+else return false, meaning anything that does return -1, because the value is not an indexof the array. */
+
 function includes(array, value) {
   if (array.length === 0) {
     return false;
--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,17 @@
 /* exported takeRight */
+/* create an array and assign it to the value of your full array, that way you can return that
+if the count is larger than the arrays length. otherwise create a for loop to go through your array
+and set the initaliazation at array.length - count you 'count' amount of letters from the end. push that into your other array
+and return it */
+function takeRight(array, count) {
+  var fullArray = array;
+  var rightArray = [];
+  if (count > array.length) {
+    return fullArray;
+  } else {
+    for (var i = array.length - count; i < array.length; i++) {
+      rightArray.push(array[i]);
+    }
+  }
+  return rightArray;
+}
