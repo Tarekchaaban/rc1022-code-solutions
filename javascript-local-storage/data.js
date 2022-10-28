bfad23f8f0b536efe76bae59c41cad47f68d7a8b
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-28 21:24:12 +0000

    javascript-local-storage completed, structure adjustment made.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -1,11 +1,12 @@
 /* exported todos */
-window.addEventListener('beforeunload', unloadHandler);
-function unloadHandler(event) {
-  var todosJSON = JSON.stringify(todos);
-  localStorage.setItem('javascript-local-storage', todosJSON);
-}
 var todos = [];
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
 if (previousTodosJSON !== null) {
   todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', unloadHandler);
+function unloadHandler(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
 }
