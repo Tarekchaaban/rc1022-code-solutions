efa3388109a0682435cba8b64bcd66a4174243a5
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-28 22:57:34 +0000

    completed array-manipulation-2 exercise.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,8 @@
 /* exported chunk */
+function chunk(array, size) {
+  var outerArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    outerArray.push(array.slice(i, i + size));
+  }
+  return outerArray;
+}
--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,1 +1,12 @@
 /* exported dropRight */
+function dropRight(array, count) {
+  var emptyArray = [];
+  var remainingArray = [];
+  if (array.length === 0) {
+    return emptyArray;
+  }
+  for (var i = 0; i < array.length - count; i++) {
+    remainingArray.push(array[i]);
+  }
+  return remainingArray;
+}
