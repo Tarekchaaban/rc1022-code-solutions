551e4d99effcf41ab55d64b63d41563a72296fcf
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-10-28 05:24:55 +0000

    need to commit to switch to main.

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,4 +1,8 @@
 /* exported drop */
+/* create an empty array for the empty array test you need to return, as well as the others.
+for the empty array, create a conditional to check if the array passed through is empty, and if so return empty array.
+for the others make a else statement with a for loop that starts at count and goes until less than the array.length, and push those into your other array.
+return that array after the for loop */
 function drop(array, count) {
   var countArray = [];
   var emptyArray = [];
