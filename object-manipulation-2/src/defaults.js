382fb09f9f96f5a39c95ba466010a9afb261e7cc
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-04 03:28:14 +0000

    basically complete.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -1,16 +1,8 @@
 /* exported defaults */
 function defaults(target, source) {
-  target = {};
   for (var key in source) {
-    if (!target[key]) {
+    if (!(key in target)) {
       target[key] = source[key];
     }
   }
 }
-var source = {
-  foo: 1,
-  bar: 2,
-  baz: 3
-};
-var target = {};
-defaults(target, source);
