be6fd1d8dfebbb25bbf9273db6ab06b479030f8d
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-22 04:05:00 +0000

    one more problem left.

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,10 @@
 /* exported intersection */
+function intersection(first, second) {
+  var intersectionArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.indexOf(first[i]) !== -1) {
+      intersectionArray.push(first[i]);
+    }
+  }
+  return intersectionArray;
+}
