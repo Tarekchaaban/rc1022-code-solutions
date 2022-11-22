7cdebfbec1052953350e4b6cd6f450afe29d4d4d
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-22 05:42:03 +0000

    98% complete.

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,15 @@
 /* exported union */
+function union(first, second) {
+  var unionArray = [];
+  for (var i = 0; i < first.length; i++) {
+    if (unionArray.indexOf(first[i]) === -1) {
+      unionArray.push(first[i]);
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (unionArray.indexOf(second[j]) === -1) {
+      unionArray.push(second[j]);
+    }
+  }
+  return unionArray;
+}
