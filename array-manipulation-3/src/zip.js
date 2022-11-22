287e22217ef836f886eea415e6d5ac1ced4d6bd5
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-22 19:42:41 +0000

    completed array manipulation 3 with adjustment to hacky code.

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,10 +1,28 @@
 /* exported zip */
-function zip(first, second) {
+/* function zip(first, second) {
   var finalArray = [];
   for (var i = 0; i < first.length; i++) {
     if (first.length > second.length) {
       first.pop();
     }
+    var pairArray = [];
+    pairArray.push(first[i]);
+    pairArray.push(second[i]);
+    finalArray.push(pairArray);
+  }
+
+  return finalArray;
+} */
+
+function zip(first, second) {
+  var finalArray = [];
+  var shortest = [];
+  if (first.length < second.length) {
+    shortest = first;
+  } else {
+    shortest = second;
+  }
+  for (var i = 0; i < shortest.length; i++) {
     var pairArray = [];
     pairArray.push(first[i]);
     pairArray.push(second[i]);
