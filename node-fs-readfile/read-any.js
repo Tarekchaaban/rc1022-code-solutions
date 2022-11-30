c1cc7ddfb9f4a61e18783713c3d7802fb9b9a651
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-11-30 23:54:28 +0000

    deleted commented out code.

--- node-fs-readfile/read-any.js
+++ node-fs-readfile/read-any.js
@@ -3,28 +3,3 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
-
-/* if (process.argv[2] === 'dijkstra.txt') {
-  fs.readFile('./dijkstra.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
-if (process.argv[2] === 'hopper.txt') {
-  fs.readFile('./hopper.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
-if (process.argv[2] === 'hipster-ipsum.txt') {
-  fs.readFile('./hipster-ipsum.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-}
-if (process.argv[2] === 'cunningham.txt') {
-  fs.readFile('./cunningham.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
-} */
