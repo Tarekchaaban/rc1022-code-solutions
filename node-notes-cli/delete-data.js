020feda1b38b2cff73cee9cff4a5efff311500e6
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-12-01 23:23:26 +0000

    completed with adjustments.

--- node-notes-cli/delete-data.js
+++ node-notes-cli/delete-data.js
@@ -1,12 +1,7 @@
 const fs = require('fs');
 const object = require('./data.json');
 if (process.argv[2] === 'delete') {
-  for (var key in object.notes) {
-    if (key === process.argv[3]) {
-      object.nextId--;
-      delete object.notes[key];
-    }
-  }
+  delete object.notes[process.argv[3]];
 
   const data = JSON.stringify(object, null, 2);
   fs.writeFile('data.json', data, 'utf-8', err => {
--- node-notes-cli/update-data.js
+++ node-notes-cli/update-data.js
@@ -1,11 +1,8 @@
 const fs = require('fs');
 const object = require('./data.json');
 if (process.argv[2] === 'update') {
-  for (var key in object.notes) {
-    if (key === process.argv[3]) {
-      object.notes[key] = process.argv[4];
-    }
-  }
+  object.notes[process.argv[3]] = process.argv[4];
+
   const data = JSON.stringify(object, null, 2);
   fs.writeFile('data.json', data, 'utf-8', err => {
     if (err) throw err;
