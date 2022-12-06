ba18acfea0cb0eb84270c44d6af21ef81e826549
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-12-06 05:41:20 +0000

    work in progress.

---
+++ express-notes-api/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const data = require('./data.json');
+const json = express.json();
+app.use(json);
+
+app.get('/api/notes', (req, res) => {
+  const notesArray = [];
+  for (var key in data.notes) {
+    notesArray.push(data.notes[key]);
+  }
+  res.json(notesArray);
+});
+
+app.listen(3000, () => {
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const idNumber = Number(req.params.id);
+  if (!(idNumber > 0)) {
+    res.status(400);
+    const err400 = { error: 'id must be a positive integer' };
+    res.send(err400);
+  } else if (!data.notes[idNumber]) {
+    const err404 = { error: 'cannot find note with id ' };
+    err404.error += idNumber;
+    res.status(404);
+    res.send(err404);
+  } else {
+    res.json(data.notes[idNumber]);
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  if (!req.body.content) {
+    res.status(400);
+    const err400 = { error: 'Request did not include content' };
+    res.send(err400);
+  } else {
+    res.status(201);
+    req.body.id = data.nextId;
+    data.notes[data.nextId] = req.body;
+    res.json(req.body);
+    data.nextId++;
+  }
+  if (!data.notes[data.nextId]) {
+    res.status(500);
+    const err500 = { error: 'An unexpected error has occured.' };
+    res.send(err500);
+  }
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const idNumber = Number(req.params.id);
+  if (!(idNumber > 0)) {
+    res.status(400);
+    const err400 = { error: 'id must be a positive integer' };
+    res.send(err400);
+  } else if (!data.notes[idNumber]) {
+    const err404 = { error: 'cannot find note with id ' };
+    err404.error += idNumber;
+    res.status(404);
+    res.send(err404);
+  } else {
+    delete data.notes[req.params.id];
+    res.sendStatus(204);
+  }
+});
