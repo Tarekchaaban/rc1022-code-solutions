d3155ccfd2f6782f7ad40234d9fd86aaf599c3e0
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-12-18 20:30:44 +0000

    react-accordion-component completed.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-export default class Accordion extends React.Component {
-  constructor(props) {
+/* export default class Accordion extends React.Component {
+   constructor(props) {
     super(props);
     this.state = {
       isClicked: false,
@@ -36,5 +36,46 @@ export default class Accordion extends React.Component {
         );
       });
     }
+  }
+} */
+
+export default class Accordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: null
+    };
+    this.setVisible = this.setVisible.bind(this);
+    this.props.topics = [
+      { topic: 'HTML', notes: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati similique ab aperiam. Illum, earum. Ab alias eveniet quia earum labore, mollitia autem? Reprehenderit, tempore autem.' },
+      { topic: 'CSS', notes: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A, odio? Voluptatem quia omnis animi. Ducimus, aperiam? Illum quae eaque tempora, minima dolorum omnis explicabo numquam.' },
+      { topic: 'JavaScript', notes: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti hic cum illum totam, architecto, amet quo distinctio temporibus delectus dolorum excepturi commodi perferendis deserunt aperiam.' }
+
+    ];
+  }
+
+  setVisible(index) {
+    if (this.state.visible === null) {
+      this.setState({ visible: index });
+    } else if (this.state.visible === index) {
+      this.setState({ visible: null });
+    } else {
+      this.setState({ visible: index });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.topics.map((topic, index) => (
+          <div key={topic.topic}>
+            <div className="border" onClick={() => this.setVisible(index)}>
+              <h1 className="center">{topic.topic}</h1>
+            </div>
+            {index === this.state.visible && <div className='border center'>{topic.notes}</div>}
+          </div>
+        ))}
+      </div>
+    );
   }
 }
