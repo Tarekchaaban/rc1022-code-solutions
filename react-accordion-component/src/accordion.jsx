98946bc04c3bf90afbc41be6bf76c689f4eb23b9
Tarek Chaaban <Chaaban_tarek@yahoo.com>
2022-12-18 07:50:42 +0000

    work in progress.

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isClicked: false };
+    this.state = {
+      isClicked: false,
+      clicked: ''
+    };
     this.handleClick = this.handleClick.bind(this);
-    /* const topics = [
+    this.props.topics = [
       { topic: 'HTML', notes: 'blahblahblah' },
       { topic: 'CSS', notes: 'blahblahblah' },
       { topic: 'JavaScript', notes: 'blahblahblah' }
 
-    ]; */
+    ];
   }
 
-  handleClick() {
+  handleClick(event) {
     this.setState({ isClicked: true });
+    const index = event.target.key;
+    this.setState({ clicked: index });
   }
 
   render() {
@@ -21,8 +26,12 @@ export default class Accordion extends React.Component {
       return this.props.topics.map((item, index) => {
         return (
           <div key={index}>
-            <h1>{item.topic}</h1>
-            <p>{item.notes}</p>
+            <div className="border" onClick={this.handleClick}>
+              <h1 className="center">{item.topic}</h1>
+            </div>
+            <div className="border">
+              <p className="center">{item.notes}</p>
+            </div>
           </div>
         );
       });
